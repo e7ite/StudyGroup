2dc5238a2d149348e7ff1ceab3308b251f00e670
Ben Marden <benvmarden@gmail.com>
2020-05-14 23:54:07 -0700

    remove unused imports

--- study_group_app/test/auth_test.dart
+++ study_group_app/test/auth_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:study_group_app/models/user.dart';
 import 'package:study_group_app/services/auth.dart';
 import 'package:study_group_app/services/services.dart';
 import 'mock/firebase_mock.dart';
-import 'mock/mock_data.dart';
 
 void main() {
   group('Auth test', () {
